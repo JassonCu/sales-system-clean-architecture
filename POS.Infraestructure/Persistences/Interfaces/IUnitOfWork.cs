429cb896060a31025f8ea23307a7cd0043cb7666
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-02-29 18:21:11 -0600

    feat: Implemento Unit of Work and Repository Design Pattern

---
+++ POS.Infraestructure/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace POS.Infraestructure.Persistences.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        // Declaración o matricula de nuestra interface a nivel de repositorio
+        ICategoryRepository Category { get; }
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
